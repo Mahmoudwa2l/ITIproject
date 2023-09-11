b19fd63618447f9e0c774a4323a38146c3c62579
Mahmoudwa2l <mahmoud.wael5@msa.edu.eg>
2023-09-12 00:54:05 +0300

    server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,43 @@
-/* 
-hnha */
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Handling the connection events
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB using Mongoose");
+});
+
+db.on("disconnected", () => {
+  console.log("Mongoose disconnected from MongoDB");
+});
+
+
+
+//midelware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users" , userRoute)
+
+app.listen(8800, () => {
+  console.log("backend server is running!");
+});
