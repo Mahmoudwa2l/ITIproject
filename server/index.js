f921d19be780a6332bd1901e7b1d24361ac1e0be
Mahmoudwa2l <mahmoud.wael5@msa.edu.eg>
2023-09-12 04:07:11 +0300

    server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -36,7 +37,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/users" , userRoute)
+app.use("/api/users" , userRoute);
+app.use("/api/auth" , authRoute);
 
 app.listen(8800, () => {
   console.log("backend server is running!");
