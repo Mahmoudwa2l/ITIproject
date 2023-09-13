9adb5650eef46bbe0e65bdce2cae3ec956b1204a
Mahmoudwa2l <mahmoud.wael5@msa.edu.eg>
2023-09-13 21:51:03 +0300

    server

--- server/routes/beats.js
+++ server/routes/beats.js
@@ -14,14 +14,75 @@ router.post("/",async (req,res) => {
 });
 
 //update a beat
+router.put("/:id", async(req,res) =>{
+    try{
 
+        const beat = await Beat.findById(req.params.id);
+        if(beat.userId === req.body.userId){
+            await beat.updateOne({$set:req.body});
+            res.status(200).json("the beat is updated");
+        }else{
+            res.status(403).json("you can update only your beat");
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 //delete a beat
+router.delete("/:id", async(req,res) =>{
+    try{
 
+        const beat = await Beat.findById(req.params.id);
+        if(beat.userId === req.body.userId){
+            await beat.deleteOne();
+            res.status(200).json("the beat is deleted");
+        }else{
+            res.status(403).json("you can delete only your beat");
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 //like a beat
-
+router.put("/:id/like" , async (req,res) =>{
+    try{
+        const beat = await Beat.findById(req.params.id);
+        if(!beat.likes.includes(req.body.userId)){
+            await beat.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("The beat has been liked");
+        }else{
+            await beat.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json("The beat has been disliked");
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 //get a beat
+router.get("/:id" , async(req,res) =>{
+    try{
+        const beat = await Beat.findById(req.params.id);
+        res.status(200).json(beat);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 
 //get all beats
+
+router.get("/all",async(req,res)=>{
+    let beatArray = [];
+    try{
+        /* const allBeats = await Promise.all() */
+        const allBeats = await Beat.find();
+        res.json(allBeats);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
