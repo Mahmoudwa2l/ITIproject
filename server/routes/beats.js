d877665245ecbb022201d656b933b8a76274a9b3
Mahmoudwa2l <mahmoud.wael5@msa.edu.eg>
2023-09-13 03:13:33 +0300

    create beat

--- server/routes/beats.js
+++ server/routes/beats.js
@@ -3,9 +3,18 @@ const Beat = require("../models/Beat");
 
 
 //create a beat
-
+router.post("/",async (req,res) => {
+    const newBeat = new Beat(req.body);
+    try {
+        const savedBeat = await newBeat.save();
+        res.status(200).json(savedBeat);
+    }catch (err){
+        res.status(500).json(err);
+    }
+});
 
 //update a beat
+
 
 //delete a beat
 
